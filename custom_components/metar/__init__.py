28a47486ae8f21ef61eab25e2e8f40771e07024b
Konstantin Bulanov <bulanov@netcracker.com>
2023-11-22 15:20:12 +0400

    Component Init

--- custom_components/metar/__init__.py
+++ custom_components/metar/__init__.py
@@ -1,1 +1,11 @@
-"""The metar component."""
+from homeassistant import core
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+    """Set up The metar component."""
+    # @TODO: Add setup code.
+    _LOGGER.info("KOBU Config %s", config)
+    return True
