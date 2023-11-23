14519165271275d732146cddf3c09aa4af483735
Konstantin Bulanov <bulanov@netcracker.com>
2023-11-23 10:22:16 +0400

    Component Init

--- custom_components/metar/__init__.py
+++ custom_components/metar/__init__.py
@@ -5,12 +5,9 @@ import voluptuous as vol
 from .const import METAR_TOKEN_FIELD, DOMAIN
 from homeassistant.config_entries import ConfigEntry
 
-METAR_SCHEMA = vol.Schema(
-    {vol.Required(METAR_TOKEN_FIELD): cv.string}
-)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
-        vol.Optional("metar"): vol.All(cv.Any, METAR_SCHEMA)
+        vol.Required(METAR_TOKEN_FIELD): cv.string
     }
 )
 
@@ -18,7 +15,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Home Heat Calc component."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data["metar"][METAR_TOKEN_FIELD] = config[DOMAIN]["metar"][METAR_TOKEN_FIELD]
+    hass.data[DOMAIN][METAR_TOKEN_FIELD] = config[DOMAIN][METAR_TOKEN_FIELD]
     entry: ConfigEntry = ConfigEntry()
     entry.title = "Metar Sensor"
     entry.data = {"code": "UWWW", "name": "KUF"}
